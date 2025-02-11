2f32bb52b0f268d021a72a21a8eabeb1ccdbbc42
mirkomerlinos <michelangelo_grasso@yahoo.com>
2025-02-11 12:07:54 +0100

    1.a

---
+++ trial_1.r
@@ -0,0 +1,18 @@
+data <- read.table("cholesterol.txt", header = TRUE, sep = " ")
+
+print(data)
+
+pdf("plots.pdf")  # Saves all following plots to a PDF file
+
+hist(data$Before, main = "Histogram of Before", col = "#191958")
+
+hist(data$After8weeks, main = "Histogram of After 8 Weeks", col = "red")
+
+qqnorm(data$Before, main = "Q-Q Plot of Before")
+qqline(data$Before, col = "blue")
+
+qqnorm(data$After8weeks, main = "Q-Q Plot of After 8 Weeks")
+qqline(data$After8weeks, col = "red")
+
+dev.off()
+
