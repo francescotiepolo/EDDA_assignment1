b066e9a0fb971f2aa12b43062e83d04418245a07
mirkomerlinos <michelangelo_grasso@yahoo.com>
2025-02-11 16:39:50 +0100

    1c added

--- trial_1.r
+++ trial_1.r
@@ -1,3 +1,5 @@
+library(boot)
+
 data <- read.table("cholesterol.txt", header = TRUE, sep = " ")
 
 print(data)
@@ -29,5 +31,27 @@ t_unpaired <- t.test(data$Before, data$After8weeks, paired = FALSE)
 print(t_paired)
 print(t_unpaired)
 
+# The data is paired since the "Before" value and the "After 8 Weeks" value represent the same subject measured twice.
 # Mann-Whitney only if the data is independent.
 # Permutations seems yes since does not assume normality
+
+# 1c
+
+differences <- data$Before - data$After8weeks
+
+# 97% CI based on normality
+CI_n <- t.test(differences, conf.level = 0.97)$conf.int
+print(CI_n)
+
+# mean difference
+boot_mean <- function(data, indices) {
+  d <- data[indices]  # Resample with replacement
+  return(mean(d))
+}
+
+# bootstrap with 10,000 resamples
+boot_results <- boot(differences, boot_mean, R = 10000)
+
+# 97% bootstrap CI
+CI_b <- boot.ci(boot_results, type = "perc", conf = 0.97)
+print(CI_b)
