fab203ca2dbb3bc616c179bfdbac3fd5d6fa3d14
francescotiepolo <francescotiepolo@icloud.com>
2025-02-14 17:04:09 +0100

    corrected 1

--- trial_1.r
+++ trial_1.r
@@ -8,6 +8,7 @@ print(data)
 
 pdf("plots_1a.pdf")  
 
+par(mfrow=c(1,2))
 hist(data$Before, main = "Histogram of Before", col = "#191958")
 
 hist(data$After8weeks, main = "Histogram of After 8 Weeks", col = "red")
@@ -18,40 +19,70 @@ qqline(data$Before, col = "blue")
 qqnorm(data$After8weeks, main = "Q-Q Plot of After 8 Weeks")
 qqline(data$After8weeks, col = "red")
 
-dev.off()
+print(cor(data$Before,data$After8weeks))
+cor.test(data$Before,data$After8weeks)
+par(mfrow=c(1,1))
+plot(data$Before~data$After8weeks); abline(lm(data$Before~data$After8weeks))
 
 # 1b
 
 t_paired <- t.test(data$Before, data$After8weeks, paired = TRUE)
-t_unpaired <- t.test(data$Before, data$After8weeks, paired = FALSE)
+wil_paired <- wilcox.test(data$Before, data$After8weeks, paired = TRUE)
 #do we need this one? 
 #var_test <- var.test(data$Before, data$After8weeks)
 #print(var_test)
 
 print(t_paired)
-print(t_unpaired)
+print(wil_paired)
+
+boxplot(data$Before, data$After8weeks, 
+        names = c("Before", "After 8 Weeks"), 
+        main = "Boxplot of Cholesterol Levels", 
+        col = c("blue", "red"))
 
 # The data is paired since the "Before" value and the "After 8 Weeks" value represent the same subject measured twice.
-# Mann-Whitney only if the data is independent.
+# Mann-Whitney only if the data is independent, here it is paired, so not applicable.
+
 # Permutations seems yes since does not assume normality
+mystat=function(x,y) {mean(x-y)}
+B=10000; tstar=numeric(B)
+for(i in 1:B){
+  datastar=t(apply(cbind(data$Before,data$After8weeks),1,sample))
+  tstar[i]=mystat(datastar[,1],datastar[,2]) }
+myt=mystat(data$Before,data$After8weeks)
+print(myt)
+
+hist(tstar, main="Permutation Test Distribution", col="lightblue", border="black")
+
+lines(rep(myt, 2), c(0, max(hist(tstar, plot=FALSE)$counts)), col="red", lwd=2)
+dev.off()
+
+pl <- sum(tstar < myt) / B
+pr <- sum(tstar > myt) / B
+p <- 2 * min(pl, pr)
+
+print(p)
+
 
 # 1c
-
-differences <- data$Before - data$After8weeks
-
 # 97% CI based on normality
-CI_n <- t.test(differences, conf.level = 0.97)$conf.int
+CI_n <- t.test(data$After8weeks, conf.level = 0.97)$conf.int
 print(CI_n)
 
-# mean difference
-boot_mean <- function(data, indices) {
-  d <- data[indices]  # Resample with replacement
-  return(mean(d))
+# Bootstrap
+
+B <- 1000
+Tstar <- numeric(B)
+
+# Bootstrap resampling
+for (i in 1:B) {
+  Xstar <- sample(data$After8weeks, replace = TRUE) 
+  Tstar[i] <- mean(Xstar)
 }
 
-# bootstrap with 10,000 resamples
-boot_results <- boot(differences, boot_mean, R = 10000)
+# Calculate the quantiles for the confidence interval
+Tstar15 <- quantile(Tstar, 0.015)
+Tstar985 <- quantile(Tstar, 0.985)
 
-# 97% bootstrap CI
-CI_b <- boot.ci(boot_results, type = "perc", conf = 0.97)
-print(CI_b)
+# Print the confidence interval
+cat("Bootstrap 97% Confidence Interval:", Tstar15, "to", Tstar985, "\n")
