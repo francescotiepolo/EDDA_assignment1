3d06b6b3bb1885b8ddfe6599dcca28cb4d86a511
francescotiepolo <francescotiepolo@icloud.com>
2025-02-15 13:06:36 +0100

    1d

--- trial_1.r
+++ trial_1.r
@@ -1,8 +1,6 @@
-library(boot)
-
 data <- read.table("cholesterol.txt", header = TRUE, sep = " ")
-
 print(data)
+set.seed(13319817)
 
 # 1a
 
@@ -66,12 +64,16 @@ print(p)
 
 # 1c
 # 97% CI based on normality
-CI_n <- t.test(data$After8weeks, conf.level = 0.97)$conf.int
-print(CI_n)
+mean_after <- mean(data$After8weeks)
+sd_after <- sd(data$After8weeks)
+n <- length(data$After8weeks)
+error <- qt(0.985, df=n-1) * (sd_after/sqrt(n))
+param_CI <- c(mean_after - error, mean_after + error)
+print(param_CI)
 
 # Bootstrap
 
-B <- 1000
+B <- 100000
 Tstar <- numeric(B)
 
 # Bootstrap resampling
@@ -86,3 +88,41 @@ Tstar985 <- quantile(Tstar, 0.985)
 
 # Print the confidence interval
 cat("Bootstrap 97% Confidence Interval:", Tstar15, "to", Tstar985, "\n")
+
+# 1d
+X = data$After8weeks
+n = length(X)
+theta_values = seq(3, 12, by=0.1)
+T_obs = max(X)
+pvals <- numeric(length(theta_values))
+
+bootstrap_test <- function(X, theta, B = 10000) {
+  # Observed statistic
+  T_obs <- max(X)
+  n <- length(X)
+  
+  # Generate bootstrap replicates of T
+  tstar <- numeric(B)
+  for (b in 1:B) {
+    xstar <- runif(n, min=3, max=theta)
+    tstar[b] <- max(xstar)
+  }
+  pr <- mean(tstar > T_obs)
+  pval <- 2 * min(pr, 1 - pr)
+  
+  return(pval)
+}
+  
+for (i in seq_along(theta_values)) {
+  pvals[i] = bootstrap_test(X, theta=theta_values[i], B = 10000)
+}
+
+not_rejected = theta_values[pvals >= 0.05]
+not_rejected
+
+hist(data$After8weeks, 
+     main="Histogram of Cholesterol Levels", 
+     xlab="Cholesterol Level", 
+     col="lightgray", 
+     border="black", 
+     prob=TRUE)
